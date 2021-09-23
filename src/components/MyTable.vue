6a2de7c5194716e7f4de5de4e1f766b7f7874ec0
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2021-09-23 10:10:34 +0300

    Striped table class

--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -1,5 +1,5 @@
 <template>
-	<table class="border border-gray-500 border-collapse">
+	<table class="border border-gray-400 border-collapse">
 		<thead>
 			<tr>
 				<th v-for="col in visibleColumns" :key="col.name">
@@ -8,7 +8,7 @@
 			</tr>
 		</thead>
 		<tbody>
-			<tr v-for="(row, i) in data" :key="i">
+			<tr v-for="(row, i) in data" :key="i" class="hover:bg-gray-200" :class="{ 'bg-gray-100': i % 2 === 1 }">
 				<td v-for="col in visibleColumns" :key="'row-i-' + col.name">
 					{{ row[col.name] }}
 				</td>
@@ -35,10 +35,10 @@ export default {
 
 <style>
 thead > tr {
-	@apply divide-x divide-gray-500 border-b border-gray-500;
+	@apply divide-x divide-gray-400 border-b border-gray-400;
 }
 tbody > tr {
-	@apply divide-x divide-gray-500;
+	@apply divide-x divide-gray-400;
 }
 th, td {
 	@apply p-2;
