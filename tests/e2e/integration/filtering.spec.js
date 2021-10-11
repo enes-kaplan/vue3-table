957c631bcd236e12236b1c64378fbe54ed7adb1f
Enes Kaplan <eneskaplan94@hotmail.com>
2021-10-11 13:46:38 +0300

    Fixed e2e tests

--- tests/e2e/integration/filtering.spec.js
+++ tests/e2e/integration/filtering.spec.js
@@ -1,21 +1,22 @@
 /// <reference types="cypress" />
 
-import { applyFilter } from '/src/helpers/filtering.js'
-
 describe('Filtering test', () => {
   beforeEach(() => {
     cy.visit('/')
   })
+
   it('counting first page rows', () => {
     cy.get('tbody')
       .find('tr')
       .should('have.length', 5)
   })
 
-  it.only('testing filtering text', () => {
+  it('testing filtering text', () => {
     const filterText = 'jo'
 
-    cy.findByRole('filter', { name: /name/  }).type(filterText)
+    cy.findByRole('filter', { name: 'name'  })
+      .type(filterText)
+
     cy.get('tbody')
       .then(body => {
         cy.wrap(body)
@@ -26,7 +27,7 @@ describe('Filtering test', () => {
           .find('tr')
           .each(row => {
             cy.wrap(row)
-              .find('td:first')
+              .findByRole('cell', { name: 'name'  })
               .invoke('text')
               .then(textVal => {
                 const test = textVal.toLowerCase()
@@ -34,5 +35,22 @@ describe('Filtering test', () => {
               })
           })
       })
+  })
+
+  it('testing filtering select', () => {
+    cy.findByRole('filter', { name: 'surname'  })
+      .select(1)
+      .invoke('val')
+      .then(selectVal => {
+        cy.get('tbody')
+          .find('tr')
+          .each(row => {
+            cy.wrap(row)
+              .findByRole('cell', { name: /surname/  })
+              .invoke('text')
+              .should('equal', selectVal)
+          })
+      })
+
   })
 })
